92eac6a3d98f6251f5340567b72960a4981dba63
Nikolay Korovaiko <korovaikon@gmail.com>
2022-02-24 03:22:46 +0000

    run cs-fixer

--- src/Payments.php
+++ src/Payments.php
@@ -1,19 +1,22 @@
-<?
+<?php
+
 namespace Utao\WeChat;
 
-class Payments {
+class Payments
+{
     public $key;
 
-    function __construct($appid, $mchid, $key) {
+    public function __construct($appid, $mchid, $key)
+    {
         $this->appid = $appid;
         $this->mchid = $mchid;
         $this->key = $key;
     }
-    
-    function sign($preorder)
+
+    public function sign($preorder)
     {
         $digest = "";
-        array_walk($preorder, function($v, $k) use (&$digest) {
+        array_walk($preorder, function ($v, $k) use (&$digest) {
             $digest .= "{$k}={$v}&";
         });
         $digest .= "key={$this->key}";
@@ -29,19 +32,18 @@ class Payments {
         return implode("\n", array_slice(explode("\n", $xmlDocument->saveXML()), 1));
     }
 
-    function xml($preorder) {
+    public function xml($preorder)
+    {
         $preorder["appid"] = $this->appid;
         $preorder["mch_id"] = $this->mchid;
         ksort($preorder);
         $signature = sign($preorder);
         $xml = new \SimpleXMLElement('<xml/>');
-        array_walk($preorder, function($k, $v) use (&$xml) {
-            $xml->addChild($v, $k); 
+        array_walk($preorder, function ($k, $v) use (&$xml) {
+            $xml->addChild($v, $k);
         });
         $xml->addChild("sign", $signature);
         $xml_str = $this->_formatXml($xml);
         return $xml_str;
     }
 }
-
-?>
